aeb3a32efc0101c45877991f80ffca99a1338f60
vikramaditya <vikramaditya.battina72@gmail.com>
2014-09-20 15:44:58 +0530

    Added some of the properties for the queue

--- Constants.js
+++ Constants.js
@@ -5,5 +5,32 @@ Constants = {
                           },
                 "stack" : {
                             "width" : 50
+                          },
+                "queue" : {
+                            "width" : 75,
+                            "front-pointer" : {
+                               "height" : 40,
+                               "pointer-gap" : 5,
+                               "stroke-width" : 3,
+                               "stroke":"#000ff0",
+                               "pointer-text-size" : 20,
+                               "pointer-text-color": "#4374FA" 
+                            },
+                            "rear-pointer" : {
+                               "height" : 40,
+                               "pointer-gap" : 5,
+                               "stroke-width" : 3,
+                               "stroke":"#000ff0",
+                               "pointer-text-size" :20,
+                               "pointer-text-color": "#4374FA"
+                            },
+                            "element" :
+                            {
+                            "text-size" : 20,
+                            "text-color" : "#4374FA",
+                            "background-color" : "#ffffff"
+                            }
                           }
+                
             }
+            
