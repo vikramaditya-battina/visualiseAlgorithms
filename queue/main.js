d288d06356e4bc8a5d236b920add4e7dfb0ebb8c
vikramaditya <vikramaditya.battina72@gmail.com>
2014-09-20 15:46:40 +0530

    Added the queue visualisation Algorithm

--- queue/main.js
+++ queue/main.js
@@ -2,6 +2,220 @@ $(document).ready(main);
 
 function main()
 {
-  createCanvas();
-  queue = new Queue(100,100,4);
+  isQueueCreated = false;  
+  initialisePropertiesOfDialog();
+  $("#ButtonEnqueue").click(onClickEnqueue);
+  $("#CreateQueue").click(onClickCreate);
+  $("#dequeue").click(onClickDequeue);
+  $("#clearqueue").click(onClickClear);
+  
 }
+function initialisePropertiesOfDialog()
+{
+   $('#overflow').dialog({
+   autoOpen: false,
+   modal : true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "blind",
+        duration: 1000
+      }
+      
+  });
+  $('#empty').dialog({
+    autoOpen: false,
+    modal : true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "blind",
+        duration: 1000
+      }
+  });
+  $('#create').dialog({
+   autoOpen: false,
+   modal : true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "blind",
+        duration: 1000
+      }
+  });
+    $('#createValidNumber').dialog({
+   autoOpen: false,
+   modal : true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "blind",
+        duration: 1000
+      }
+  });
+  
+    $('#validSize').dialog({
+   autoOpen: false,
+   modal : true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "blind",
+        duration: 1000
+      }
+  });
+}
+function measuringCanvasSize()
+{
+   var windowSize = $(window).width();
+   var availableCanvasSize = windowSize - windowSize * 0.06; // excluding 3% margin on left and right
+   
+   var givenWidth = Constants["queue"]["width"];
+   var capacity = availableCanvasSize/givenWidth;
+   capacity = Math.floor(capacity);
+   return { "capacity" : capacity , "canvasSize" : availableCanvasSize};
+}
+function createQueue(len)
+{
+   var result = measuringCanvasSize();
+   var capacity =  result && result["capacity"];
+   var canvasSize = result && result["canvasSize"];
+   if( capacity == undefined || len > capacity-2)
+   {
+      return false;
+   }
+   canvas = Raphael("canvas",canvasSize,500);
+    var x = (capacity - len)/2 + (capacity - len)%2;
+    var queueWidth = Constants["queue"]["width"];
+    var startx = x* queueWidth;
+    var starty = Constants["canvas"]["height"]/3;
+    queue = new Queue(startx,starty,len);
+    return true;
+}
+
+function onClickEnqueue()
+{
+    var enQueueValue = $('#enqueuevalue').val();
+    console.log(typeof enQueueValue);
+       if(!isQueueCreated)
+       {
+          $("#create").dialog("open");
+          return;
+       }
+       if(enQueueValue == undefined || enQueueValue.trim()==="")
+       {
+         alert("please provide value");
+         return;
+       }
+       $("#overlay").fadeIn(100);
+       queue.enqueue(enQueueValue,callback);
+       function callback(status)
+       {
+          $("#overlay").fadeOut(100);
+         if( status === false)
+         {
+            $("#overflow").dialog("open");
+          }
+       }
+}
+
+function onClickDequeue()
+{
+   if(!isQueueCreated)
+       {
+          $("#create").dialog("open");
+          return;
+       }
+       $("#overlay").fadeIn(100);
+       queue.dequeue(callback)
+       function callback(result)
+       {
+          $("#overlay").fadeOut(100);
+          if( result == false)
+          $("#empty").dialog("open");
+          else
+          {
+             $("#TextFieldDequeueValue").val(result);
+          }
+       }
+}
+
+function onClickCreate()
+{
+  
+     if( isQueueCreated )
+     {
+        queue.removeQueue();
+        isQueueCreated = false;
+        onClickCreate();
+     }
+     else
+     {
+       var len = $('#TextFieldQueueLen').val();
+       console.log(len);
+       console.log(typeof len);
+       var len = parseInt(len);
+       console.log(typeof len);
+       if(isNaN(len))
+       {
+          $("#createValidNumber").dialog("open");
+          return;
+       }
+        var flag = createQueue(len);
+        if(flag)
+        {
+          isQueueCreated = true;
+        }
+        else
+        {
+           $("#validSize").dialog("open"); 
+        }
+     }     
+}
+
+function onClickClear()
+{
+    if( isQueueCreated )
+    {  
+       queue.removeQueue();
+    }    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- queue/queueCreation.js
+++ queue/queueCreation.js
@@ -3,8 +3,102 @@ function Queue(startx,starty,len)
       var queueContext = this;
       function initialise()
       {
-          queueContext.len = len;
-          queueContext.arr = new MyArray(startx,starty,Constants["queue"]["width"],Constants["queue"]["width"],len);          
+          queueContext.size = len;
+          queueContext.arr = new MyArray(startx,starty,Constants["queue"]["width"],Constants["queue"]["width"],len,Constants["queue"]);
+          queueContext.front = 0;
+          queueContext.rear = 0;
+          queueContext.count = 0;
+          queueContext.frontPointer = null;
+          queueContext.rearPointer = null;
+          queueContext.enqueue = enqueue;
+          queueContext.dequeue = dequeue;
+          queueContext.removeQueue = removeQueue ;
+          queueContext.frontPointer = queueContext.arr.createPointer(queueContext.front+1,"front","front",true,Constants["queue"]["front-pointer"]);
+          queueContext.rearPointer =  queueContext.arr.createPointer(queueContext.rear+1,"rear","rear",false,Constants["queue"]["rear-pointer"]); 
       }
       initialise();
+      
+      function enqueue(value,callback)
+      {
+       
+        if(isFull())
+        {
+           if( typeof callback === 'function')
+            {
+               callback(false);
+               return;
+            }  
+        }
+        queueContext.count =  queueContext.count+1;
+        queueContext.arr.fillColor(queueContext.front+1,"#94AFF7");
+        queueContext.arr.setText(queueContext.front+1,value);
+        queueContext.front = (queueContext.front+1)%queueContext.size;
+        queueContext.frontPointer.move(queueContext.front+1,successcallback);
+        function successcallback()
+        {
+           if( typeof callback === 'function')
+            {
+               callback(true);
+            }                     
+        }
+      }
+      
+      
+      function dequeue(callback)
+      {
+         if(isEmpty())
+         {
+             if( typeof callback === 'function')
+             {
+                callback(false);
+                return;
+             }
+         }
+        
+        queueContext.count =  queueContext.count-1;
+        var dequeuedvalue = queueContext.arr.elements[queueContext.rear].getText();
+        queueContext.arr.setText(queueContext.rear+1,"");
+        queueContext.arr.fillColor(queueContext.rear+1,"#ffffff");
+        queueContext.rear = (queueContext.rear+1)%queueContext.size;
+        queueContext.rearPointer.move(queueContext.rear+1,successcallback);            
+         
+        function successcallback()
+        {
+          if( typeof callback === 'function')
+             {
+                callback(dequeuedvalue);
+             }
+        }         
+         
+      }
+      
+      function isFull()
+      {
+        if(  (queueContext.front == queueContext.rear) && queueContext.count == queueContext.size)
+          {
+            return true;   
+          }
+          return false;
+        
+       }
+       
+       function isEmpty()
+       {
+         if(  (queueContext.front == queueContext.rear) && queueContext.count == 0) 
+         {
+            return true;
+         }
+         return false;
+       }
+      
+       function removeQueue()
+       {
+          queueContext.arr.removeArray();   
+       }
+      
+      
+      
+      
+      
+      
 }
